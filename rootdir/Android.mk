7820e13798ae9748b68f34f71fd4483b27e74a41
ShreehariVaasishta <shreehari9481@gmail.com>
2020-11-02 08:27:26 +0000

    Revert "X01BD: rootdir: Move init.recovery.qcom.rc out of root"
    This reverts commit 62aa8a88968bf21ceae91d649c7a6d99d5484eb7.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,7 +40,7 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
