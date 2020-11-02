b34a17fc98950c8a0d72863ffd876a6db3fe1af2
ShreehariVaasishta <shreehari9481@gmail.com>
2020-11-02 04:43:24 +0000

    Revert "include gapps"
    This reverts commit 9bdfb25e2132a71b1d821270087b8b6898658e2a.

--- device.mk
+++ device.mk
@@ -457,6 +457,3 @@ PRODUCT_PACKAGES += \
 
 # Call the proprietary setup
 $(call inherit-product, vendor/asus/X01BD/X01BD-vendor.mk)
-
-# Gapps
-$(call inherit-product-if-exists, vendor/google/gms/config.mk)
